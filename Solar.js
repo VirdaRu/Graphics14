208c4080aaf07c96d11d15ab5c66dfefe8906939
VirdaRu <lars.kingma99@gmail.com>
2021-09-24 10:11:51 +0200

    scene

--- Solar.js
+++ Solar.js
@@ -106,8 +106,7 @@ function main() {
   const geoSphere = new THREE.SphereGeometry( 15, 32, 16);
   
   function Bol(geoSphere, color, x, y, z,) {
-    const material = new THREE.MeshBasicMaterial({color, transparent: true, opacity: 0.75});
-    
+    const material = new THREE.MeshPhongMaterial({color, transparent: true, emissive: 0xFFFF00});    
     
     const sphere = new THREE.Mesh(geoSphere, material);
     scene.add(sphere);
@@ -116,8 +115,6 @@ function main() {
     sphere.position.y = y;
     sphere.position.z = z;
 
-
-    return sphere;
   }
   
   const spheres = [
